23f9662475a7384308a6eabc3af4a2ca1fcb4302
AlishaJahan <jahanalisha17@gmail.com>
2025-05-13 19:26:47 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+      
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println("Current Date and Time: " + now);
+
+       
+        System.out.println("Keep going, you're doing great! 🚀");
+    }
+}
